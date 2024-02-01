4bbab963b6996f7151c98bc0a450117490cb3c50
noneedhere <zidanerosyidi@gmail.com>
2024-02-01 08:55:22 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modifier;
+
+/**
+ *
+ * @author Asus
+ */
+public class main {
+    
+    public static void main (String[] args){
+        person mPerson = new person();
+        mPerson.setName("zidane");
+        
+        System.out.println("Person Name:"+mPerson.getName());
+    }
+}
---
+++ person.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modifier;
+
+/**
+ *
+ * @author Asus
+ */
+public class person {
+    public String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+
+    
+}
